add correct dimension to softmax and add some prints during training

neural_net_handling/network_architectures/segment_net.py
@@ -7,7 +7,7 @@ class SegmentDetNet(nn.Module):
         super().__init__()
         self.model = torchvision.models.resnet18(pretrained=True, progress=True)
         self.model.fc = nn.Linear(512, num_classes)
-        self.last_layer = nn.Softmax(num_classes)
+        self.last_layer = nn.Softmax(dim=1)
 
         for param in self.model.parameters():
             param.requires_grad = False