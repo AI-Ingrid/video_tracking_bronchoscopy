6c64d196657266cc8602024ed18ac6820923c65a
Ingrid Kolderup <ingrid.kolderup@gmail.com>
2022-11-16 09:46:30 +0100

    train segment det net with 3 generations on idun

--- parameters.py
+++ parameters.py
@@ -17,8 +17,8 @@ names_file_path = root_directory_path + f"/{dataset_type}_branch_number_name.txt
 
 
 # CNN
-network_type = "direction_det_net"  # {"direction_det_net", "segment_det_net"}
-num_bronchus_generations = 1  # {1, 2, 3, 4}
+network_type = "segment_det_net"  # {"direction_det_net", "segment_det_net"}
+num_bronchus_generations = 3  # {1, 2, 3, 4}
 epochs = 5
 batch_size = 32
 learning_rate = 5e-4
