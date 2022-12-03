ae0f9ab34acddbcfcdd604846e0a5b16d1930673
Ingrid Kolderup <ingrid.kolderup@gmail.com>
2022-12-03 13:33:44 +0100

    uncomment stuff

--- main.py
+++ main.py
@@ -84,12 +84,12 @@ def main():
     test = test_dataloader
 
     # Test CNN model
-    print("---- TRAINING ----")
-    train_loss, train_acc = compute_loss_and_accuracy(train, neural_net, torch.nn.CrossEntropyLoss())
-    print("---- VALIDATION ----")
-    val_loss, val_acc = compute_loss_and_accuracy(validation, neural_net, torch.nn.CrossEntropyLoss())
-    print("---- TEST ----")
-    test_loss, test_acc = compute_loss_and_accuracy(test, neural_net, torch.nn.CrossEntropyLoss())
+    #print("---- TRAINING ----")
+    #train_loss, train_acc = compute_loss_and_accuracy(train, neural_net, torch.nn.CrossEntropyLoss())
+    #print("---- VALIDATION ----")
+    #val_loss, val_acc = compute_loss_and_accuracy(validation, neural_net, torch.nn.CrossEntropyLoss())
+    #print("---- TEST ----")
+    #test_loss, test_acc = compute_loss_and_accuracy(test, neural_net, torch.nn.CrossEntropyLoss())
 
     # Plot test images with predicted and original label on it
     plot_predictions_test_set(test, trainer, test_plot_path)
