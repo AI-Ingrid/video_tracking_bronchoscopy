5405293ce0131e2e83fe56eb04f02c2bc10e25a3
Ingrid Kolderup <ingrid.kolderup@gmail.com>
2022-12-11 18:49:48 +0100

    wip confusion matrix

--- neural_net_handling/utils/neural_net_utilities.py
+++ neural_net_handling/utils/neural_net_utilities.py
@@ -182,7 +182,7 @@ def compute_f1_score(test_set, trainer):
 
 def plot_confusion_matrix(test_set, trainer, plot_path):
     all_predicted_labels = []
-    all_original_labels = np.array()
+    all_original_labels = np.array([])
     for X_batch, Y_batch in test_set:
         X_batch_cuda = to_cuda(X_batch)
 
